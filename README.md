2dddb782ac765739587d85df2929525580f89b49
tri.nguyen <tri.nv@terra-drone.co.jp>
2022-12-14 14:05:53 +0700

    Init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Angular-Heroes
+# Angular version 15.0.0
+# Node v16.15.1
+# Npm v8.11.0
+# Start npm install
---
+++ src/app/hero.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { Hero } from "./hero";
+import { HEROES } from "./mock-heroes";
+import { MessageService } from "./message.service";
+
+@Injectable({ providedIn: "root" })
+export class HeroService {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+  ) {}
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+}
